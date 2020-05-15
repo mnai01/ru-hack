0ff6b62647d4a7dbb1418695ef1411ae838fc663
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-15 11:23:40 -0400

    changed styling

--- src/components/UserInfo/userinfo.js
+++ src/components/UserInfo/userinfo.js
@@ -1,43 +1,51 @@
 import React from "react";
 import Media from "react-bootstrap/Media";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import avi from "../../assets/avi.png";
 const userInfo = () => (
   <div>
-    <Media>
-      <img
-        width={100}
-        height={100}
-        className="align-self-start mt-5 ml-5 mr-5"
-        src={avi}
-        alt="Generic placeholder"
-      />
-      <Media.Body className="mt-2">
-        <h5>Bryan B</h5>
-        <p>United States, New York</p>
-        <p>Speaks: English, Spanish</p>
-        <p>Interested in: Chinese</p>
-        <p>Speaks: English, Spanish</p>
-      </Media.Body>
-    </Media>
     <Container fluid>
-      <Row className="pt-4 pl-4">
+      <Media className=" m-2">
+        <img
+          width={100}
+          height={100}
+          className="m-5"
+          src={avi}
+          alt="Generic placeholder"
+        />
+        <Media.Body className="pt-3">
+          <Row>
+            <Col>
+              <Row>
+                <h5 className="pl-3 mr-4">Bryan B</h5>
+                <p>(last seen 5 min ago)</p>
+              </Row>
+              <h6>United States, New York</h6>
+              <h6>Speaks: English, Spanish</h6>
+              <h6>Interested in: Chinese</h6>
+              <h6>Speaks: English, Spanish</h6>
+            </Col>
+          </Row>
+        </Media.Body>
+      </Media>
+      <Row className="p-3">
         <h2> If you can travel anywhere in the world where would it be?</h2>
       </Row>{" "}
-      <p className="pt-4 pl-4">Canada, those moose man....</p>
-      <Row className="pt-4 pl-4">
+      <p className="p-3">Canada, those moose man....</p>
+      <Row className="p-3">
         <h2> What is your dream job?</h2>
       </Row>{" "}
-      <p className="pt-4 pl-4">Astronaut</p>
-      <Row className="pt-4 pl-4">
+      <p className="p-3">Astronaut</p>
+      <Row className="p-3">
         <h2> What is your favorite food? </h2>
       </Row>{" "}
-      <p className="pt-4 pl-4">Tacos</p>
-      <Row className="pt-4 pl-4">
+      <p className="p-3">Tacos</p>
+      <Row className="p-3">
         <h2> What are your hobbies? </h2>
       </Row>{" "}
-      <p className="pt-4 pl-4">Shitty design</p>
+      <p className="p-3"> I like to knit</p>
     </Container>
   </div>
 );
