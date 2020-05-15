7efc3ebfb5950dbd2c5aa93e3d598723d59b9aff
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-15 15:22:25 -0400

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import UserInfo from "./components/UserInfo/userinfo";
+import LandingPage from "./components/LandingPage/LandingPage";
 function App() {
   return (
     <div className="App">
       <Header />
-      <UserInfo />
+      <LandingPage />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,6 @@ export const Header = () => (
         <Nav.Link>Search</Nav.Link>
         <Nav.Link>Message</Nav.Link>
         <Nav.Link>Chat now</Nav.Link>
-        <Nav.Link>Settings</Nav.Link>
       </Nav>
       <Nav>
         <Nav.Link>Profile</Nav.Link>
