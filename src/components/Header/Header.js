6fa837ae78327753efe5a425fa392bcb6d3102f4
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-15 11:20:51 -0400

    Made header collapsible

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,20 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 export const Header = () => (
-  <Navbar bg="dark" variant="dark" sticky="top">
+  <Navbar bg="dark" variant="dark" sticky="top" expand="sm">
     <Navbar.Brand href="">InterPals</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link>Search</Nav.Link>
-      <Nav.Link>Message</Nav.Link>
-      <Nav.Link>Chat now</Nav.Link>
-      <Nav.Link>Settings</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link>Profile</Nav.Link>
-    </Nav>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link>Search</Nav.Link>
+        <Nav.Link>Message</Nav.Link>
+        <Nav.Link>Chat now</Nav.Link>
+        <Nav.Link>Settings</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link>Profile</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 );
 
