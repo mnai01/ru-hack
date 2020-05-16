3a269daa73e5f6c9343f1d303d6b93a611634416
mnai01 <imatlak03@gmail.com>
2020-05-16 16:19:28 -0400

    added mock data

---
+++ src/testDataCall.js
@@ -0,0 +1,42 @@
+export const testdatacall = async () => {
+  return [
+    {
+      language_interests: ["English", "Spanish"],
+      status: "online",
+      last_login: "01/03/2020",
+      username: "yoloswag420",
+      password: "sassfdasfas",
+      email: "IanMatlak@gmail.com",
+      gender: "male",
+      country: "USA",
+      city: null,
+      state: null,
+      firstname: "Ian",
+      lastname: "Matlak",
+      speaks: [],
+      photo: "https://www.w3schools.com/w3images/avatar2.png",
+      bio:
+        "Hello nice to meet you all hopefully we can talk soon test test testtesttesttesttesttesttesttest",
+      friends: ["tom1234", "yolo1234"],
+    },
+    {
+      language_interests: ["English", "Spanish"],
+      status: "online",
+      last_login: "02/04/2010",
+      username: "yoloswag420",
+      password: "sassfdasfas",
+      email: "IanMatlak@gmail.com",
+      gender: "male",
+      country: "USA",
+      city: null,
+      state: null,
+      firstname: "Ian",
+      lastname: "Matlak",
+      speaks: [],
+      photo: "https://www.w3schools.com/w3images/avatar2.png",
+      bio:
+        "Hello nice to meet you all hopefully we can talk soon test test testtesttesttesttesttesttesttest",
+      friends: ["tom1234", "yolo1234"],
+    },
+  ];
+};
