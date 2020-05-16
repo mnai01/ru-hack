f195fbf91d010b5745aa55508d313893c34f000c
mnai01 <imatlak03@gmail.com>
2020-05-16 02:06:10 -0400

    Added links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,24 +3,26 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 export const Header = () => (
-<<<<<<< HEAD
-  <Navbar bg="dark" variant="dark" sticky="top" expand="sm">
-    <Navbar.Brand href="">
-      <Link to={"/"}>Interpals</Link>
-    </Navbar.Brand>
-=======
   <Navbar bg="dark" variant="dark" sticky="top" expand="sm" className="mb-4">
-    <Navbar.Brand href="">FarFriends</Navbar.Brand>
->>>>>>> search-page
+    <Navbar.Brand href="">
+      <Link to="/">FarFriends</Link>
+    </Navbar.Brand>
+
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link>Search</Nav.Link>
-        <Nav.Link>Message</Nav.Link>
+        <Nav.Link>
+          <Link to="/SearchPage">Search Page</Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/Messages">Message</Link>
+        </Nav.Link>
         <Nav.Link>Chat now</Nav.Link>
       </Nav>
       <Nav>
-        <Nav.Link>Profile</Nav.Link>
+        <Nav.Link>
+          <Link to="/Profile">Profile</Link>
+        </Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
