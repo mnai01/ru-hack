ec2b03a7708746a576807ee956392b14fa807834
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-16 22:04:49 -0400

    Testing api routes

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import UserFilter from "../UserFilter/UserFilter";
 import UserTiles from "../UserTiles/UserTiles";
 import { Container, Row, Col } from "react-bootstrap";
-
+import { UserContext } from "../../userContext";
 const SearchPage = () => {
+  const { onlineUsers, loading, getOnlineUsers } = useContext(UserContext);
+
+  useEffect(() => {
+    getOnlineUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container fluid className="p-5">
       <Row>
