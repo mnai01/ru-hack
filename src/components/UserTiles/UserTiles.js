d6d534e17381db6fbbc83277c092c16e58290d3f
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-16 22:05:27 -0400

    Maps users array to list of UserTiles

--- src/components/UserTiles/UserTiles.js
+++ src/components/UserTiles/UserTiles.js
@@ -1,26 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
 import UserTile from "./UserTile/UserTile";
-import data from "../../assets/Data.json";
+import { UserContext } from "../../userContext";
 
 const UserTiles = () => {
-<<<<<<< HEAD
-  // let msg = userContext(userContext);
-=======
-  let msg = useContext(userContext);
->>>>>>> 2a890f6f5390be3180799cfb4b6e8806f74a788d
+  const { onlineUsers } = useContext(UserContext);
+  const users = onlineUsers.map((user) => {
+    return (
+      <li>
+        <UserTile user={user} />
+      </li>
+    );
+  });
 
-  useEffect(() => {
-    setData(data);
-    console.log(data);
-    //console.log("this is msg " + msg);
-  }, []);
-
-  const [data, setData] = useState();
-  return (
-    <div>
-      <UserTile />
-    </div>
-  );
+  return <ul>{users}</ul>;
 };
 
 export default UserTiles;
